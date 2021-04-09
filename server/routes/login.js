d312324c59d5301af1dfdf904a33f854fa1c7271
Maksymilian Biruk <mexberuk@gmail.com>
2021-04-09 16:50:11 +0200

    add logging route

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,1 +1,19 @@
 import express from 'express';
+import Users from '../models/User.js';
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const foundUser = await Users.findOne({username: req.body.username});
+
+    if (!foundUser){
+        res.status(404).json({message: "account not found"});
+    } else {
+        if (foundUser.password === req.body.password){
+            res.status(200).json(foundUser); //returns User object from mongodb
+        } else {
+            res.status(401).json({message: "wrong password"});
+        }
+    }
+});
+
+export default router;
