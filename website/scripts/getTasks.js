92e7ef69c2c0d7a7fe2342fb834bba4e231ee049
Maksymilian Biruk <mexberuk@gmail.com>
2021-04-10 18:28:46 +0200

    add log out, add info after task delete, add info after task add

--- website/scripts/getTasks.js
+++ website/scripts/getTasks.js
@@ -1,22 +1,27 @@
 let list = document.querySelector('ol');
+let taskStatus = document.getElementById('task-status');
 
 //getting an _id that has been passed with the user logging
 //to specify from which user pull the tasks data
 let _id = localStorage.getItem('_id');
 let url = `http://localhost:2000/tasks/${_id}`;
 
-let removeTask = async (item, index) => {
+let removeTask = (item, index) => {
     let data = {
         index: index
     }
 
-    await fetch(url, {
+    fetch(url, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    });
+    })
+    .then(res => res.json())
+    .then(data => {
+        taskStatus.innerText = data.message;
+    })
 
     item.remove();
 }
