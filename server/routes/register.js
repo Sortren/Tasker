fda704a5af5bb133bcc18c4beadb39d50a05913f
Maksymilian Biruk <mexberuk@gmail.com>
2021-04-10 19:51:23 +0200

    improve register validation

--- server/routes/register.js
+++ server/routes/register.js
@@ -9,19 +9,19 @@ router.post('/', async (req, res) => {
 
     //Data Validation
     const { error } = registerValidation(req.body);
-    
     if (error) return res.status(400).send(error.details[0].message); 
 
     //Check if the user is already in database
     const emailExist = await Users.findOne({email: req.body.email});
-    
     if (emailExist) return res.status(400).send('Email already exists');
 
+    const usernameExist = await Users.findOne({username: req.body.username});
+    if (usernameExist) return res.status(400).send('Username already exists');
 
+    //If validation went correctly: 
     //Hashing the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
 
 
     const user = new Users({
