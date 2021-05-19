b0908fc5cdd497204c080613387ed31c48d17345
Maksymilian Biruk <mexberuk@gmail.com>
2021-05-19 20:57:04 +0200

    add better auth html sending

--- server/routes/authorize.js
+++ server/routes/authorize.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import Users from '../models/User.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import express from "express";
+import Users from "../models/User.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
 
+router.get("/:authToken", async (req, res) => {
+  const foundUser = await Users.findOne({ authToken: req.params.authToken });
 
-router.get('/:authToken', async (req, res) =>{
-    const foundUser = await Users.findOne({authToken: req.params.authToken});
-
-    //if auth token date to expire is greater than current time, user will be authenticated
-    if (foundUser.authTokenExpires > Date.now()){
-        await Users.updateOne(
-            {authToken: foundUser.authToken},
-            {$set: {
-                authorized: 'true'
-            }});
-        console.log(__dirname);    
-        res.sendFile('C:/Dokumenty/PROJECTS PROGRAMMING/js_projects/nodejs_projects/TO-DO-List/website/emailConfirm.html');
-    }
-})
+  //if auth token date to expire is greater than current time, user will be authenticated
+  if (foundUser.authTokenExpires > Date.now()) {
+    await Users.updateOne(
+      { authToken: foundUser.authToken },
+      {
+        $set: {
+          authorized: "true",
+        },
+      }
+    );
+    res.sendFile(
+      path.join(__dirname, "../responseHTML/authorize/authorized.html")
+    );
+  }
+});
 
 export default router;
